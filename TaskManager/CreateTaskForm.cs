7577f4b84bcd0eca7b8225e308c6f91a1e39cc62
S4Ones <98596226+S4Ones@users.noreply.github.com>
2023-09-09 23:33:03 +0400

    Add files via upload
    Notification

--- TaskManager/CreateTaskForm.cs
+++ TaskManager/CreateTaskForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace TaskManager
@@ -20,5 +21,31 @@ namespace TaskManager
     {
       InitializeComponent();
     }
-  }
+
+        private void createTaskButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Уведомление о создании новой задачи.
+        /// </summary>
+        /// <param name="task">Новая задача</param>
+        private void ShowNotification(ListView task)
+        {
+            var notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = SystemIcons.Information;
+            notifyIcon.Visible = true;
+
+            notifyIcon.BalloonTipTitle = "Новая задача: " + task.Title;
+            notifyIcon.BalloonTipText = "Описание: " + task.Disciption + Environment.NewLine +
+                                        "Приоритет: " + task.Status.ToString() + Environment.NewLine +
+                                        "Дедлайн: " + task.Deadlines + Environment.NewLine +
+                                        "Список ответственных: " + string.Join(", ", task.Responsible);
+
+            notifyIcon.ShowBalloonTip(3000);
+
+            notifyIcon.Dispose();
+        }
+    }
 }
