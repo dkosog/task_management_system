be2b0412a1fc63ad76a716fac6e24fba7d56dfd8
ForKill <gtateam@mail.ru>
2023-09-09 10:11:00 +0400

    Создан класс ответственного.

---
+++ TaskManager/Responsible/Responsible.cs
@@ -0,0 +1,64 @@
+﻿namespace TaskManager.Responsible
+{
+    /// <summary>
+    /// Класс ответственных.
+    /// </summary>
+    public class Responsible
+    {
+        #region Поля и свойства
+
+        /// <summary>
+        /// Имя ответственного.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Почта ответственного.
+        /// </summary>
+        public string Email { get; set; }
+
+        #endregion
+
+        #region Конструктор
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="name">Имя ответственного.</param>
+        /// <param name="email">Почта ответственного.</param>
+        public Responsible(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        #endregion
+
+        #region Методы
+
+        public override string ToString()
+        {
+            return $"{Name} - {Email}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Responsible responsible = obj as Responsible;
+
+            if (responsible == null)
+                return false;
+
+            return Name == responsible.Name && Email == responsible.Email;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Email.GetHashCode();
+        }
+
+        #endregion
+    }
+}
